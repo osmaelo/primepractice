30d6538253842dffc04c8e01f10273f2db56d0aa
Ismael Olea <osmaelo@gmail.com>
2018-06-06 22:39:52 -0700

    First commit

---
+++ rbprimes.rb
@@ -0,0 +1,27 @@
+# test1.rb
+# require 'rb-readline'
+# require 'byebug'
+
+def prime?(num)
+  (2...num).each do |idx|
+    if num % idx == 0
+      return false
+    end
+  end
+  true
+end
+
+def primes(num_primes)
+  ps = []
+  num = 2
+  while ps.count < num_primes
+    ps << num if prime?(num)
+    num += 1
+    # debugger
+  end
+  ps
+end
+
+if __FILE__ == $PROGRAM_NAME
+  puts primes(5)
+end
