89afae592775de68b57351d7b23dbdbd4fb83720
Ismael Olea <osmaelo@gmail.com>
2018-06-08 17:41:49 -0700

    Learning Java

--- jvprimes.java
+++ jvprimes.java
@@ -1,9 +1,14 @@
 public class jvprimes {
   public static void main(String[] args) {
     System.out.println("Hi there!");
+    int count = 1;
+    while (count < 5) {
+      System.out.println("Count is: " + count);
+      count++;
+    }
   }
 
-  public static boolean isPrimeNum(int num) {
-
-  }
+  // public static boolean isPrimeNum(int num) {
+  //
+  // }
 }
