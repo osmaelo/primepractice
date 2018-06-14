8103da877d0135c8a3e3b4f714325c9b9c246bec
Ismael Olea <osmaelo@gmail.com>
2018-06-13 18:03:33 -0700

    Progress in  Java

--- jvprimes.java
+++ jvprimes.java
@@ -1,15 +1,26 @@
 public class jvprimes {
   public static void main(String[] args) {
-
+    // Insert functions in here to run them.
+    // System.out.println(isPrime(2));
+    int[] primeList = new int[10];
+    System.out.println(primeList.length);
+    System.out.println(primeList);
   }
   public static boolean isPrime(int num) {
-    for (int i = 2; i <= num; i++) {
+    for (int i = 2; i < num; i++) {
       if (num % i == 0) {
         return false;
       }
     }
     return true;
   }
-}
 
-jvprimes.isPrime(7);
+  // public static int[] primes(int numPrimes) {
+  //   int[] primeList = new int[numPrimes];
+  //   while (primeList.length < numPrimes) {
+  //
+  //   }
+  //   return primeList;
+  //
+  // }
+}
