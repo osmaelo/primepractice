a3da79379a40491a79106c110ddfc0c2bf3bd0dc
Ismael Olea <osmaelo@gmail.com>
2018-07-26 20:29:28 -0700

    Java Update

--- jvprimes.java
+++ jvprimes.java
@@ -2,7 +2,6 @@
 // Compile then run
 // Notes that should go here in preparation for algorithm study
 // The final method/function of java should go in the first main method/function. That is how everything will be run
-// About 100% done with Book
 
 public class jvprimes {
   public static void main(String[] args) {
