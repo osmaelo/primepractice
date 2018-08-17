24367189508b5adfa8544011fd3665d005ec3a7b
Ismael Olea <osmaelo@gmail.com>
2018-08-16 22:28:08 -0700

    Update jvprimes.java

--- jvprimes.java
+++ jvprimes.java
@@ -3,7 +3,7 @@
 // Notes that should go here in preparation for algorithm study
 // The final method/function of java should go in the first main method/function. That is how everything will be run
 // Working on making cloned repos work
-// Book is ready, still at 0%
+// Book is ready, still at 0% again
 
 public class jvprimes {
   public static void main(String[] args) {
